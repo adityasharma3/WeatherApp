3d88a114bad123a18cd0bbf29c84c07e634b8877
Aditya Sharma <aditya.sharma9301@gmail.com>
2020-07-03 14:21:30 +0530

    simplified app.py

--- app.py
+++ app.py
@@ -5,35 +5,9 @@ import urllib.request
 app = Flask(__name__)
 
 @app.route('/' , methods = ['POST' , 'GET'])
+def index():
+    return render_template('weather.html' , data=[{'name':'New Delhi'} , {'name' : 'Mumbai'} , {'name' : 'Kolkata'} , {'name' : 'Bangalore'} , {'name' : 'Chennai'} , {'name' : 'Hyderabad'} , {'name' : 'Bhopal'}])
 
-def weather():
-    if request.method == 'POST':
-        city = request.form('city')
-    else:
-        city = 'Mumbai'
 
-    api = "64e1b6ec71msh79f17be5fb5db5dp1ecd69jsnb974c96d020c"
-    source = urllib.request.urlopen("https://community-open-weather-map.p.rapidapi.com/find")
-
-    ##response = requests.request("GET", url, headers=headers, params=querystring)
-        # converting JSON data to a dictionary 
-    list_of_data = json.loads(source) 
-  
-    # data for variable list_of_data 
-    data = { 
-        "country_code": str(list_of_data['sys']['country']), 
-        "coordinate": str(list_of_data['coord']['lon']) + ' ' 
-                    + str(list_of_data['coord']['lat']), 
-        "temp": str(list_of_data['main']['temp']) + 'k', 
-        "pressure": str(list_of_data['main']['pressure']), 
-        "humidity": str(list_of_data['main']['humidity']), 
-    } 
-    print(data) 
-    return render_template('index.html', data = data) 
-  
-  
-  
-if __name__ == '__main__': 
-    app.run(debug = True) 
-
-  
+if __name__ == '__main__'
+app.run(debug = True)
